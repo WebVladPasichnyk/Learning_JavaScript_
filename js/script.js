cfc3076127bdca407842114c9b00e7fa718b5151
Vlad Pasichnyk <vladislav.pasichnyk@gmail.com>
2023-05-06 18:00:21 +0300

    First commit

---
+++ js/script.js
@@ -0,0 +1,103 @@
+// console.log('Hello!');
+"use strict";
+
+// b = 15;
+// console.log(b);
+
+// let number = 5; 
+// const leftBorderWidth = 1;
+
+// number = 10;
+// console.log(number);
+
+// const obj = {
+//    a: 50
+// };
+
+// obj.a = 10;
+// console.log(obj);
+
+// console.log(name);
+// var name = 'John'
+
+// {
+//    var result = 60;
+// }
+// console.log(result);
+
+// const width = 50;
+// const length = 100;
+
+// console.log("Ширина автомобіля: " + width + ", а довжина: " + length);
+
+// let number = 4.6;
+
+// console.log(-4/0);
+
+// const obj = {
+//    name: "Vlad",
+//    age: 18,
+//    isMarried: false
+// };
+// console.log(obj.name, obj.age);
+
+// let arr = ['papka', 10, 'vlad', {}];
+// console.log(arr[3], arr[0]);
+
+// const arr = ['a', 'b', 'c']  //записоно у вигляді МАСИВА;
+
+// const arrObj = {  //записано у вигляді ОБ'ЄКТА;
+//    0: 'a',
+//    1: 'b',
+//    2: 'c'
+// };
+
+// arr[10] = '3456';
+
+// console.log(arr);
+//також можна через крапку, але ЛИШЕ букви;
+
+// arrObj.b = '1234'; //тут задали властивість 'b';
+// arrObj['b'] = '1234'; //можна і так записувати;
+// console.log(arrObj['b']);
+
+// const storeName = 'Razafff.prod';
+
+// const storeDescription = {
+//     budget: 1000,
+//     employees: ['Vlad', 'Ura', 'Razaf'],
+//     products: {
+//       Lamborghini: 150,
+//       Ferrari: 200
+//     },
+//     open: true
+// };
+
+// const result = confirm('You are 18 years old?');
+// console.log(result);
+
+// const answer = +prompt('Write please your current age:', '');
+// console.log(typeof(answer));
+
+// const answers = [];
+
+// answers[0] = prompt('Як вас звати?');
+// answers[1] = prompt('Скільки вам років?');
+// answers[2] = prompt('З якого ви міста?');
+
+// document.write(answers);
+
+// console.log('arr' + ' - object');
+// console.log(4 + + '5');
+
+let incr = 10,
+discr = 10;
+++incr;
+discr--;
+
+document.write(++incr, ',', discr);
+
+const isCheck = false,
+      isClose = true;
+
+console.log(isCheck && isClose);
